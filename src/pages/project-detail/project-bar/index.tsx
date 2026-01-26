aec73a7683239e198a7e9864d769eadce05cb696
VN-PhanQuocBinh <binh26042005@gmail.com>
2026-01-26 15:34:15 +0700

    style: simplify return statement in ProjectBar component

--- src/pages/project-detail/project-bar/index.tsx
+++ src/pages/project-detail/project-bar/index.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 function ProjectBar() {
-  return (
-    <div className='h-project-bar-height bg-indigo-100'>ProjectBar</div>
-  )
+  return <div className="h-project-bar-height bg-indigo-100">ProjectBar</div>;
 }
 
-export default ProjectBar
+export default ProjectBar;
